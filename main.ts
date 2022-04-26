b546506f26ce14e35de64c01c4319a0f412899b3
袖手蹲 <5830098+pdtopdog@users.noreply.github.com>
2022-04-26 11:17:46 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,36 @@
+let time_text = ""
+let hour_12 = 0
+let minute_text = ""
+esp8266.init(SerialPin.P1, SerialPin.P2, BaudRate.BaudRate115200)
+esp8266.connectWiFi("zmhm", "12312300")
+if (esp8266.isWifiConnected()) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
+esp8266.initInternetTime(8)
+if (esp8266.isInternetTimeInitialized()) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
+basic.clearScreen()
+basic.forever(function () {
+    esp8266.updateInternetTime()
+    if (esp8266.isInternetTimeUpdated()) {
+        if (esp8266.getMinute() < 10) {
+            minute_text = "0" + esp8266.getMinute()
+        } else {
+            minute_text = convertToText(esp8266.getMinute())
+        }
+        if (esp8266.getHour() > 12) {
+            hour_12 = esp8266.getHour() - 12
+        } else {
+            hour_12 = esp8266.getHour()
+        }
+        time_text = "" + hour_12 + ":" + minute_text
+        basic.showString(time_text)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
